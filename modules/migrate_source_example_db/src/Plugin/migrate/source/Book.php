8e7136148a08c8b837adcfa2d16e2941965bd867
Martins Kajins <martins@Martinss-MacBook-Pro.local>
2016-01-14 15:03:06 +0200

    #111206180. Node migration. Author, user and taxonomy migration is not included.

---
+++ modules/migrate_source_example_db/src/Plugin/migrate/source/Book.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate_source_example_db\Plugin\migrate\source\Book.
+ */
+
+namespace Drupal\migrate_source_example_db\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Extract users from database.
+ *
+ * @MigrateSource(
+ *   id = "migrate_source_example_db_book"
+ * )
+ */
+
+class Book extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    return $this->select('migrate_source_example_db_books', 'b')
+      ->fields('b', array('bid', 'title',  'body', 'body_format'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields['bid'] = $this->t('Bid');
+    $fields['title'] = $this->t('Title');
+    $fields['body'] = $this->t('Body');
+    $fields['body_format'] = $this->t('Body format');
+    // $fields['image'] = $this->t('Image');
+    // $fields['attributes'] = $this->t('Attributes');
+    // $fields['user'] = $this->t('User');
+    $fields['created'] = $this->t('Created');
+    $fields['alias'] = $this->t('Alias');
+    return $fields;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return array(
+      'bid' => array(
+        'type' => 'integer',
+        'alias' => 'b',
+      ),
+    );
+  }
+
+}
