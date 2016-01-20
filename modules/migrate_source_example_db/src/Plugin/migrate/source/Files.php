412f02128a2c5d458d812fb203603aa27e4e7231
Guntis Jakovins <guntis.jakovins@wunderkraut.com>
2016-01-20 09:54:27 +0200

    Attempt at base path declaration

--- modules/migrate_source_example_db/src/Plugin/migrate/source/Files.php
+++ modules/migrate_source_example_db/src/Plugin/migrate/source/Files.php
@@ -49,4 +49,19 @@ class Files extends SqlBase {
     );
   }
 
+    /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+
+    // Get filename property value.
+    $filename = $row->getSourceProperty('filename');
+    // Prepare base path.
+    $base_path = trim($this->configuration['source_base_path'], '/');
+    // Set "full_path" property to point to the full path of the image.
+    $base_module_path = drupal_get_path('module', 'migrate_source_example');
+    $row->setSourceProperty('full_path', $base_module_path . '/' . $base_path . '/' . $filename);
+  }
+
 }
