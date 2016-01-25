13121f3efc268cdfb36a46a3f34c8e0417233793
Martins <martins.kajins@wunderkraut.com>
2016-01-25 14:49:17 +0200

    #111205736. Image migration. Can't copy files from source/images to public://.

--- modules/migrate_source_example_xml/src/Plugin/migrate/source/Image.php
+++ modules/migrate_source_example_xml/src/Plugin/migrate/source/Image.php
@@ -9,6 +9,7 @@ namespace Drupal\migrate_source_example_xml\Plugin\migrate\source;
 use Drupal\migrate\Entity\MigrationInterface;
 use Drupal\migrate_source_xml\Plugin\migrate\source\XmlBase;
 use Drupal\migrate_source_xml\Plugin\migrate\source\MigrateXmlReader;
+use Drupal\migrate\Row;
 
 
 /**
@@ -86,6 +87,20 @@ class Image extends XmlBase {
     }
 
     return $count;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+    // Get filename property value.
+    $filename = $row->getSourceProperty('filename');
+    // Prepare base path.
+    $base_path = trim($this->configuration['source_base_path'], '/');
+    // Set "full_path" property to point to the full path of the image.
+    $base_module_path = drupal_get_path('module', 'migrate_source_example');
+    $row->setSourceProperty('full_path', $base_module_path . '/' . $base_path . '/' . $filename);
   }
 
 }
