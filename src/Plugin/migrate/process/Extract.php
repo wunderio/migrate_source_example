b0c365e0b38933a229e40de31fc138866b19f26b
Guntis Jakovins <guntis.jakovins@wunderkraut.com>
2016-01-29 11:52:04 +0200

    #111205760-Initial xml book migration and id fix for image and user migrations. Extract plugin created. Stil needs work on category and author.

---
+++ src/Plugin/migrate/process/Extract.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate_source_example\Plugin\migrate\process\Extract.
+ */
+
+namespace Drupal\migrate_source_example\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * @MigrateProcessPlugin(
+ *   id = "extract",
+ * )
+ */
+class Extract extends ProcessPluginBase {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $result = implode(" ", [$value[$this->configuration['number']]]);
+    return $result;
+  }
+}
