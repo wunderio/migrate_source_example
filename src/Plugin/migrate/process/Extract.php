a201b9dbcf57bf1d642d2b05fc80d2711f64bfd2
Raimonds Kalnins <raimonds.kalnins@wunderkraut.com>
2016-02-01 23:40:14 +0200

    #111205760 - Extract migrate process plugin updated to throw an exception if expected value is not set in the given array.

--- src/Plugin/migrate/process/Extract.php
+++ src/Plugin/migrate/process/Extract.php
@@ -8,6 +8,7 @@
 namespace Drupal\migrate_source_example\Plugin\migrate\process;
 
 use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateException;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
 
@@ -17,7 +18,22 @@ use Drupal\migrate\Row;
  * )
  */
 class Extract extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws MigrateException
+   */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!isset($this->configuration['key'])) {
+      throw new MigrateException('Key of expected array element is not defined.');
+    }
+
+    if (!isset($value[$this->configuration['key']])) {
+      throw new MigrateException(sprintf('Element with key "%s" is not set in given value.', $this->configuration['key']));
+    }
+
     return $value[$this->configuration['key']];
   }
+
 }
